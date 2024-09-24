7786ab00fa37e2f089fc4308dc90de605a7926b9
sharathdoes <justabountyhunter935@gmail.com>
2024-09-25 02:16:29 +0530

    venvadded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,26 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor code into a separate chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          // Add more conditions here if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Set the limit to 1 MB (adjust as needed)
+  },
+});
